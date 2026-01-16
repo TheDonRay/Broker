8e10b9708948589a6f956bf01d07994050a7695f
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-16 18:16:12 -0500

    working on the organization + ai response and analysis

--- backend/controller/organizesend.controller.js
+++ backend/controller/organizesend.controller.js
@@ -1,2 +1,10 @@
 //import the data from the service folder that calls the external api here as such
 const { fetchStockData } = require("../services/marketstack.service.js");
+const OpenAi = require('openai'); 
+const client = new OpenAi({ 
+    apiKey: process.env.OPENAI_KEY
+}); 
+
+const organizeAnalysis = async (req, res) => { 
+    // first i want to set up a try and catch case here 
+}
--- backend/routes/organizesend.route.js
+++ backend/routes/organizesend.route.js
@@ -1,4 +1,8 @@
 const express = require("express");
-const organizeandsend = express.Router();
+const organizeandsend = express.Router(); 
+const oasController = require('../controller/organizesend.controller.js'); 
+
+// get requeuest becase we are getting the data 
+organizeandsend.get('/analysis', oasController); 
 
 module.exports = organizeandsend;

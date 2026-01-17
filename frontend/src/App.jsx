8a3cb176132034e4a6502165dcad59135934fed9
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-16 23:24:28 -0500

    working on the login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage.jsx";
+import SignUpPage from "./components/Signuppage.jsx"; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Example: <Route path="/" element={<Home />} /> */}
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Homepage />} /> 
+        <Route path="/" element={<Signuppage />} /> 
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React from "react"; 
+import { useNavigate } from 'react-router-dom'; 
 import "../styles/Homepage.css";
 
-export default function Homepage() {
-  return( 
-    <> 
-        <div className="login"> 
-            <h2 className='login-title'>Welcome To Broker</h2> 
-            <p className='subtextabout-broker'>Hesitant on a stock? Let Broker break the hesitation.</p>
-        </div>
+export default function Homepage() { 
+
+    function signUpPage() { 
+
+    }
+  return (
+    <>
+      <div className="login">
+        <h2 className="login-title">Welcome To Broker</h2>
+        <p className="subtextabout-broker">
+          Hesitant on a stock? Let Broker break the hesitation.
+        </p> 
+        <form> 
+            <div className="Text-area"> 
+                <input 
+                    type="text" 
+                    id="username" 
+                    name="Username" 
+                    placeholder="Username"
+                    className="textinput" 
+                />
+            </div> 
+            <div className='Text-area'>  
+                <input 
+                    type="password" 
+                    id="password"  
+                    name="Password" 
+                    placeholder="Password" 
+                    className="textinput"
+                /> 
+            </div> 
+            <input 
+                type="submit" 
+                value="Login" 
+                className="btn" 
+            /> 
+ 
+            <input 
+                type="submit" 
+                value="Sign Up" 
+                className="btn" 
+            />
+        </form>
+      </div>
     </>
   );
 }

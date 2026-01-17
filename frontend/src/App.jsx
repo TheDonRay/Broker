159f3b7df679945526bbbf0bdb82337c98782957
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-16 22:55:21 -0500

    working on the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { Homepage } from "./components/Homepage.jsx"; 
 
 function App() {
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Example: <Route path="/" element={<Home />} /> */} 
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/components/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'; 
+
+export default function Homepage() {  
+
+    return( 
+        <> 
+        <h1>Welcome to Broker</h1>
+        </>
+    )
+}

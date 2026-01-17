d49da55299f6f0f1d3fc01e57906cb0a38ecf539
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-16 23:10:23 -0500

    working on the frontend

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import "../styles/Homepage.css";
 
 export default function Homepage() {
-  return <></>;
+  return( 
+    <> 
+        <div className="login"> 
+            <h2 className='login-title'>Welcome To Broker</h2> 
+            <p className='subtextabout-broker'>Hesitant on a stock? Let Broker break the hesitation.</p>
+        </div>
+    </>
+  );
 }

bdda6c1815c3db4036e84288b046dc3c873c67f1
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-19 18:57:48 -0500

    working on authenthication part 1 which is creating the schema plan

---
+++ backend/model/user.model.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose'); 
+
+// TODO: create the schema below to hold user login informatin based of what the frontend asks for.  
+// keep in mind this is where the collection (users) is going to be stored inside the cluster. 

9d5c7a1390230e0c83662cd6a89084ef05076e16
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-19 18:51:36 -0500

    connection to mongodb was successful

--- backend/config/database.connection.js
+++ backend/config/database.connection.js
@@ -1,1 +1,18 @@
-const mongoose = require('mongoose')
+require('dotenv').config(); 
+const mongoose = require('mongoose'); 
+
+const MONGO_URI = process.env.MONGODB_URI; 
+// create the connection function here as such 
+const brokerclusterconnection = async () => { 
+    // implement a try and catch case here as such 
+    try { 
+        await mongoose.connect(MONGO_URI); 
+        console.log('Connected to MongoDB successfully'); 
+    } catch (error) { 
+        console.error('Error connecting to MongoDB:', error.message); 
+        process.exit(1); 
+    }
+} 
+
+//export the connection 
+module.exports = brokerclusterconnection; 
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const app = require("./app.js");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT; 
+// call the database here as such 
+const brokerclusterconnection = require('./config/database.connection.js'); 
+//invoke the function since the db function needs to be called when the server starts 
+brokerclusterconnection(); 
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);

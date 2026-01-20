b1cce15843483a4cb52e237818c2fc398f66b660
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-19 22:34:44 -0500

    working on it

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -2,3 +2,7 @@ const mongoose = require('mongoose');
 
 // TODO: create the schema below to hold user login informatin based of what the frontend asks for.  
 // keep in mind this is where the collection (users) is going to be stored inside the cluster. 
+const users = new mongoose.Schema({ 
+    //defining the schema here which is going to get imported into one of the controllers. 
+
+})

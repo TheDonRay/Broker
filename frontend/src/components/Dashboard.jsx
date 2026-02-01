6db4fd73f3e76c182a44707ab800ffe142cd8def
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-01 13:57:23 -0500

    broker brand logo created

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+export default function DashboardPage() {
+  return (
+    <div className="container-welcome-div">
+      {/*Gets the users credential specifically the username and displays onto the frontend for the dashboard. */}
+    </div>
+  );
+}
